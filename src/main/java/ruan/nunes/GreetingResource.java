97bd6409f9acb63bd924986e8ae1fc6894d54418
RuanNunes <ruanschwanz@gmail.com>
2023-12-07 22:04:28 -0300

    live 07/12/23

--- src/main/java/ruan/nunes/GreetingResource.java
+++ src/main/java/ruan/nunes/GreetingResource.java
@@ -4,13 +4,14 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public Response hello() {
+        return Response.ok("Hello from RESTEasy Reactive").build();
     }
 }
